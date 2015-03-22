3fa5d2ae8341edb379366febbf678b10419acc77
linsonzhao <linson.zhao@hotmail.com>
2015-03-23 07:18:22 +1100

    MySQLite

---
+++ MySQLite/src/com/android/mysqlite/Comment.java
@@ -0,0 +1,28 @@
+package com.android.mysqlite;
+
+public class Comment {
+	private long id;
+	private String comment;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+	
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	@Override
+	public String toString() {
+		return comment;
+	}
+
+}
